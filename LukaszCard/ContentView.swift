23f631700f481dbdbe6a28367b2cda8e0411b154
Lukasz Tatarynowicz <tatarynowicz92@gmail.com>
2019-12-23 19:40:14 +0100

    final commit

--- LukaszCard/ContentView.swift
+++ LukaszCard/ContentView.swift
@@ -10,7 +10,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color(red:0.00, green:0.72, blue:0.58)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("Lukasz")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit).frame(width: 150.0, height: 200.0)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                Text("Lukasz Tatarynowicz")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+48 694 479 977", imageName: "phone.down.circle.fill")
+                InfoView(text: "tatarynowicz@icloud.com", imageName: "envelope.fill")
+            }
+        }
     }
 }
 
@@ -19,3 +39,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ LukaszCard/InfoView.swift
@@ -0,0 +1,34 @@
+//
+//  InfoView.swift
+//  LukaszCard
+//
+//  Created by Łukasz Tatarynowicz on 23/12/2019.
+//  Copyright © 2019 Łukasz Tatarynowicz. All rights reserved.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .foregroundColor(.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
